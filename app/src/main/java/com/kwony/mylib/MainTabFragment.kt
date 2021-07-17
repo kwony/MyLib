6b19639770baedfc510e7fb1876f53c3e9383f45
Hyeonkwon Cho <kwony.cho@snowcorp.com>
2021-07-17 13:56:18 +0900

    feature: bottom tab

--- app/src/main/java/com/kwony/mylib/MainTabFragment.kt
+++ app/src/main/java/com/kwony/mylib/MainTabFragment.kt
@@ -16,11 +16,50 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainTabFragment: BaseFragment<FragmentMainTabBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentMainTabBinding = FragmentMainTabBinding::inflate
 
+    private val adapter by lazy {
+        MainTabPagerAdapter(childFragmentManager, lifecycle)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.viewpager2.adapter = MainTabPagerAdapter(childFragmentManager, lifecycle)
+        binding.viewpager2.adapter = adapter
         binding.viewpager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.viewpager2.isUserInputEnabled = false
+
+        binding.newTab.setOnClickListener {
+            binding.viewpager2.setCurrentItem(adapter.getPosition(TabType.NEW), false)
+        }
+        binding.searchTab.setOnClickListener {
+            binding.viewpager2.setCurrentItem(adapter.getPosition(TabType.SEARCH), false)
+        }
+        binding.bookmarkTab.setOnClickListener {
+            binding.viewpager2.setCurrentItem(adapter.getPosition(TabType.BOOK_MARK), false)
+        }
+
+        binding.viewpager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                when (adapter.getType(position)) {
+                    TabType.NEW -> {
+                        binding.newTab.setTextColor(0xff000000.toInt())
+                        binding.searchTab.setTextColor(0xffd8d8d8.toInt())
+                        binding.bookmarkTab.setTextColor(0xffd8d8d8.toInt())
+                    }
+                    TabType.BOOK_MARK -> {
+                        binding.bookmarkTab.setTextColor(0xff000000.toInt())
+                        binding.searchTab.setTextColor(0xffd8d8d8.toInt())
+                        binding.newTab.setTextColor(0xffd8d8d8.toInt())
+                    }
+                    TabType.SEARCH -> {
+                        binding.searchTab.setTextColor(0xff000000.toInt())
+                        binding.bookmarkTab.setTextColor(0xffd8d8d8.toInt())
+                        binding.newTab.setTextColor(0xffd8d8d8.toInt())
+                    }
+                }
+            }
+        })
     }
 
     private class MainTabPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
@@ -35,6 +74,10 @@ class MainTabFragment: BaseFragment<FragmentMainTabBinding>() {
                 TabType.SEARCH -> SearchFragment()
             }
         }
+
+        fun getPosition(tabType: TabType) = items.indexOfFirst { it == tabType }
+
+        fun getType(position: Int) = items[position]
     }
 
     enum class TabType {
